5af610766555d54bd45cb162425ad3489be93fec
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2020-11-03 20:08:25 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/pgbezerra/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgbezerra.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
