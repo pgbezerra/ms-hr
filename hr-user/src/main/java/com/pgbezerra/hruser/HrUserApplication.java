3829d61ed9015de53611d6adad2aeb67bc10502b
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2020-11-09 14:14:09 -0300

    Database seeding

--- hr-user/src/main/java/com/pgbezerra/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/pgbezerra/hruser/HrUserApplication.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication  {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
